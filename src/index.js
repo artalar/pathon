0611860994961c40ec016a83d052d95a91c490c7
artalar <artalar@ya.ru>
2018-06-23 20:34:57 +0300

    fix exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-export { path } from './pathon';
-export { mutablePreset, immutablePreset } from './presets';
+const { path } = require('./pathon');
+const { mutablePreset, immutablePreset } = require('./presets');
+
+module.exports.path = path;
+module.exports.mutablePreset = mutablePreset;
+module.exports.immutablePreset = immutablePreset;
