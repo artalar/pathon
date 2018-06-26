669fd49a17e167a35b727316bd1cc3bf471b2817
artalar <artalar@ya.ru>
2018-06-26 08:27:48 +0300

    add tests

--- src/pathon.test.js
+++ src/pathon.test.js
@@ -2,7 +2,7 @@ const path = require('./pathon').path;
 const immutablePreset = require('./presets').immutablePreset;
 
 describe('pathon', () => {
-  it('path structure', () => {
+  it('methods', () => {
     const pathRoot = path('root', { child: true });
     expect(typeof pathRoot.set).toBe('function');
     expect(typeof pathRoot.get).toBe('function');
@@ -12,7 +12,6 @@ describe('pathon', () => {
     expect(typeof pathRoot.getPath).toBe('function');
     expect(typeof pathRoot.getPathFull).toBe('function');
     expect(typeof pathRoot.path).toBe('function');
-    expect(typeof pathRoot.set).toBe('function');
 
     const pathChild = pathRoot.path('child');
     expect(typeof pathChild.get).toBe('function');
@@ -23,9 +22,22 @@ describe('pathon', () => {
     expect(typeof pathChild.getPathFull).toBe('function');
     expect(typeof pathChild.path).toBe('function');
   });
-  it('memorized path', () => {
+  it('memorized child', () => {
     const pathRoot = path('root', { child: true });
     expect(pathRoot.path('child')).toBe(pathRoot.path('child'));
+  });
+  it('subscriptions', () => {
+    const pathRoot = path('root', { child: false });
+    let subscriptionToRoot = false;
+    let subscriptionToChild = false;
+
+    pathRoot.watch(state => (subscriptionToRoot = state.child));
+    pathRoot.path('child').watch(state => subscriptionToChild = state);
+
+    pathRoot.path('child').set(true);
+
+    expect(subscriptionToRoot).toBe(true);
+    expect(subscriptionToChild).toBe(true);
   });
   it('root path state', () => {
     const initialState = {};
